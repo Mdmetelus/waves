152557bf59501d3703f9baf413640a049824579d
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-27 03:48:04 -0500

    "some user datatype setups added, "

--- server/server.js
+++ server/server.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const app= express();
 
-// app["m-search"]()
+
+
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE)
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//--------------------------------------------
+// -----     USERS    ------    USERS -------
+//-------------------------------------------
+
+app.post('/api/users/register', (req, res) => {
+    res.status(200);
+})
+
 
 const port = process.env.PORT || 3004;
 
 app.listen(port, () => {
-    console.log(`The Server is running on Port ${port}`)
+    console.log(`The Server is running on Port ${port} !
+    And will refresh thanks to Nodemon. :))`)
 })
